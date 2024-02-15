a26b07b69d347b9289a72f2a61e8b0793d2dae24
Dev-Narpheesah <adeolanafisat81@gmail.com>
2024-02-15 15:44:38 +0100

    update

---
+++ loop/script.js
@@ -0,0 +1,28 @@
+//  LOOP
+
+// While loop
+// let i = 0;
+// while(i < 10) {s
+//     console.log(i)
+//     i++
+// }
+
+// FOR LOOP
+
+// for([initialization]; [condition]; [final expression]) {
+    // Statement
+// }
+
+for (let i = 0; i < 10; i++) {
+    console.log(i)
+}
+
+for (let i = 1; i <= 8; i++) {
+    console.log(i)
+}
+
+const colors = ["red", "green", "blue", "yellow", "violet"];
+
+for(let i = 0; i < colors.length; i++) {
+    console.log(colors[i])
+}
