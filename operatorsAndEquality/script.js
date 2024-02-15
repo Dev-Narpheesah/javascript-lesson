3f8fb2cd91a0cb7bac66571ef5e0d7f51b484243
Dev-Narpheesah <adeolanafisat81@gmail.com>
2024-02-15 14:18:45 +0100

    update

--- operatorsAndEquality/script.js
+++ operatorsAndEquality/script.js
@@ -1,25 +1,120 @@
-const a = 10;
-const b = 30;
-let result = 0;
+// ARITHMETIC OPERATORS 
 
-result = a + b;
-console.log(result);
+// const a = 10;
+// const b = 30;
+// let result = 0;
 
-result = a - b;
-console.log(result);
+// result = a + b;
+// console.log(result);
 
-result = a / b;
-console.log(result);
+// result = a - b;
+// console.log(result);
 
-result = a * b;
-console.log(result);
+// result = a / b;
+// console.log(result);
 
-result = a % b;
-console.log(result);
+// result = a * b;
+// console.log(result);
 
-result = a ** b;
-console.log(result);
+// result = a % b;
+// console.log(result);
+
+// result = a ** b;
+// console.log(result);
+
+// let number = 5;
+// number++
+// console.log(number);
+
+// COMPARISON OPERATORS
+
+// <
+// >
+// =
+// <=
+// >=
+//  !=
+
+// const a = 10;
+// const b = 13;
+
+// let result = a > b;
+// console.log(result);
+
+// result = a < b;
+
+// console.log(result);
+
+// result = a === b;
+
+// console.log(result);
+
+// result = a <= b;
+
+// console.log(result);
+
+// result = a >= b;
+
+// console.log(result);
+
+// result = a != b;
+
+// console.log(result);
+
+// Strict (===) and loose (===) equality
+
+// result = 0 === "0";
+// console.log(result);
+
+// result = 0 == "0";
+// console.log(result);
+
+// LOGICAL OPERATOR
+// AND (&&) to check if all operrands are true
+// OR (||) to check if one operrand is true return true
+// NOT (!) reverse the boolean
+
+// let answer = 0;
+
+// answer = true && true;
+
+// console.log(answer);
+
+// answer = true && false;
+
+// console.log(answer);
+
+// OR
+
+// answer = true || true;
+
+// console.log(answer);
+
+// answer = true || false;
+
+// console.log(answer);
+
+// NOT
+
+// answer = !true
+
+// console.log(answer);
+
+
+// ASSIGNMENT OPERATOR
 
 let number = 5;
-number++
+
 console.log(number);
+
+number += 5
+
+console.log(number);
+
+let word = "hello";
+
+console.log(word);
+
+word += ", i am mr Aliyu";
+
+console.log(word);
