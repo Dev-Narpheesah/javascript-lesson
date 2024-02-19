e908d8e8c34fcbe66f43071b8b19e86131358a8a
Dev-Narpheesah <adeolanafisat81@gmail.com>
2024-02-19 12:07:47 +0100

    second commit

---
+++ functions/script.js
@@ -0,0 +1,51 @@
+//  Function declaration.
+
+function square(number) { 
+    // statement
+}
+
+square() 
+
+// Function Expression
+const nameFunction = function(params) {}
+
+// Arrow Function
+const arrowFn = () => {}
+
+
+function sayHello(firstName) {
+    console.log(`Hello ${firstName}`);
+}
+
+sayHello(`HayZed`)
+sayHello(`Class Captain 🙅‍♂️`)
+sayHello(`Nafisah`)
+
+
+// const division = (number) => {
+//     return number / number;
+// }
+
+// const answer = division(4)
+// console.log(answer);
+
+const square2 = number => {return number * number}
+const square3 = number => number * number
+
+function add(number) {
+    return number + number
+}
+
+const sum = add(2);
+console.log(sum)
+
+
+const para = (age, height) => {
+    return (age - height);
+}
+
+const sol = para (50, 20)
+console.log(sol);
+
+
+// NESTED is a function written inside another function
