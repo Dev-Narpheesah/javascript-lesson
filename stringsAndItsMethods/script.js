95ec7de1a4aed89b5c093c66c6c1c27b6246a265
Dev-Narpheesah <adeolanafisat81@gmail.com>
2024-02-20 14:57:47 +0100

    third commit

---
+++ stringsAndItsMethods/script.js
@@ -0,0 +1,40 @@
+//  Types of strings
+
+// single qoute ('')
+const world = 'ï am a boy'
+
+// double qoute ("")
+const words = "ï am a boy"
+
+
+// backticks(``)
+const worlds = `ï am a boy`
+
+// console.log(world, words, worlds)
+
+
+const age = 12
+const school = 'DLT'
+const name = `Eef`
+
+const sentence = `my name is ${name}, I am ${age} years old and i go to ${school} school`
+
+console.log(sentence)
+
+
+const math = "2 + 2"
+
+const mathematics = `${2 + 2}`
+
+const lines = `1
+2
+3
+4
+5`
+
+console.log(lines);
+
+const numerals = `Hello i'm Aliyu and iam from "kogi state"`
+console.log(numerals)
+
+// Checking the lenght of a string
--- trickyConcept/script.js
+++ trickyConcept/script.js
@@ -1,21 +1,21 @@
 //  Global scope,
-const name = "Zainab";
-const age = 17;
-const school = "Agege Primary School, Orile";
+// const name = "Zainab";
+// const age = 17;
+// const school = "Agege Primary School, Orile";
 
-const one  = () => {
-    return `${name} of ${age} years attented ${school}`;
-}
+// const one  = () => {
+//     return `${name} of ${age} years attented ${school}`;
+// }
 
-const answer = one()
-console.log(answer)
+// const answer = one()
+// console.log(answer)
 
-const file = () => {
-    return `${name} likes eating ${age} wraps of Semo in ${school}`
-}
+// const file = () => {
+//     return `${name} likes eating ${age} wraps of Semo in ${school}`
+// }
 
-const sentence = file();
-console.log(sentence)
+// const sentence = file();
+// console.log(sentence)
 
 // Local scope
 const localScope = () => {
@@ -64,5 +64,60 @@ const m = 2
 
 //  CLOSURE // The scope regarding a function inside a function
 
+const outerFunction = () => {
+    const ben = 5
+
+    const innnerFunction = () => {
+        const man = 7
+
+        return ben + man
+    }
+
+      const final = innnerFunction()
+     console.log(final * 2)
+
+}
+
+ outerFunction()
 
 
+// const mFunction = () => {
+//     const b = 10
+
+//     if (b = 10) {
+//         return b >= 10 ? 20 : 100;
+//     }
+
+//     }
+//     const finalFunction= mFunction() 
+//     console.log(finalFunction * 4)
+
+
+const firstFunc = () => {
+    const div = 2
+
+
+    const secondFunc = () => {
+        const condition = true
+        let certainNumber
+        const mult = 4
+
+
+
+        if (condition) {
+            certainNumber = 5
+        }
+
+        const ans = certainNumber * mult
+        return ans
+    }
+
+    const initialAnswer = secondFunc()
+
+    const finalAnswer = initialAnswer / div
+
+    console.log(finalAnswer) 
+}
+
+
+      firstFunc()
