a60c762eae724bdb16a015387da0e9dd000676ba
Dev-Narpheesah <adeolanafisat81@gmail.com>
2024-02-27 11:24:09 +0100

    update

--- objects/script.js
+++ objects/script.js
@@ -61,3 +61,85 @@ const dog = {
     }
 }
  dog.listAllProperties()
+
+
+ const myObject = {
+    myMethod1: () => {},   // arrow function
+    myMethod2: function() {},   // function declaration
+ }
+
+ const animal = {
+    name: "Fluffy orange",
+    age: 10,
+
+    bark: () => {
+        console.log("Woof woof!")
+    }
+ }
+ console.log(animal.name);
+ animal.bark();
+
+
+//  ojects.keys()
+
+ const employees = {
+    boss: "Besh",
+    secretary: "Azeez",
+    sales: "Zainab",
+    account: "Bola",
+    cleaner: "Ade"
+
+ }
+
+
+const employeeInfo = Object.keys(employees)
+console.log(employeeInfo);
+
+//  ojects.value()
+
+const session = {
+    id: 1,
+    date: "27-feb-2024",
+    device: "Mobile",
+    browser: "Chrome"
+}
+
+const sessionValue = Object.values(session)
+console.log(sessionValue);
+
+
+//  ojects.entries()
+
+const operatingSystem = {
+    name: "Linux",
+    version: 18.04,
+    license: "Open source"
+}
+
+const operatingSystemInfo = Object.entries(operatingSystem)
+console.log(operatingSystemInfo);
+
+const user = {
+    userName: "John",
+    password: 12345
+}
+
+const userInfo = Object.freeze(user)
+console.log(userInfo);
+
+userInfo.userName = "Soliu"
+console.log(userInfo);
+
+
+
+const userOne = {
+    userName: "John",
+    password: 12345
+}
+
+const userInfoOne = Object.seal(userOne)
+console.log(userInfoOne);
+
+userInfoOne.userName = "Az"
+userInfoOne.age = 32
+console.log(userInfoOne);
