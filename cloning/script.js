d9ecd082ce8094fab42ce7d0f699c29b617dec5b
Dev-Narpheesah <adeolanafisat81@gmail.com>
2024-02-28 10:54:52 +0100

    update

--- cloning/script.js
+++ cloning/script.js
@@ -71,3 +71,13 @@ cloneObj.address.city = "Elejigbo";
 
 console.log("originalObj", originalObj);
 console.log("cloneObj", cloneObj);
+
+
+// Spreading Operator
+const arr1 = [1,2,3,4];
+const arr2 = [5,6,7,8];
+const arr3 = [8,9]
+
+const merged = [...arr1, ...arr2, ...arr3];
+
+console.log(merged);
