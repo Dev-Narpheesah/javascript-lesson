d10ba39307ed39f7aab00abcbee0fb39b06a24bd
Dev-Narpheesah <adeolanafisat81@gmail.com>
2024-02-29 09:32:29 +0100

    update

--- oop/script.js
+++ oop/script.js
@@ -1,1 +1,9 @@
-cgvbhjnmk,l;
+const rectangleArea = function(length, breadth) {
+    return length * breadth;
+};
+
+
+const length = 4;
+const breadth = 5;
+const area = rectangleArea(length, breadth);
+console.log(area);
