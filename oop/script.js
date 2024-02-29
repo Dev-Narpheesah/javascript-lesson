d357ea2004fa7398cc220bb0541b2ca2fc8ee89d
Dev-Narpheesah <adeolanafisat81@gmail.com>
2024-02-29 14:21:58 +0100

    update

--- oop/script.js
+++ oop/script.js
@@ -1,12 +1,12 @@
-const rectangleArea = function(length, breadth) {
-    return length * breadth;
-};
+// const rectangleArea = function(length, breadth) {
+//     return length * breadth;
+// };
 
 
-const length = 4;
-const breadth = 5;
-const area = rectangleArea(length, breadth);
-console.log(area);
+// const length = 4;
+// const breadth = 5;
+// const area = rectangleArea(length, breadth);
+// console.log(area);
 
 
 //OOP --. Object Oriented Programming.
@@ -98,3 +98,92 @@ const newCohort$ = new Cohort("Eef", 24, "Pounded yam", 4567, new Date("february
 
 const new$Cohort = new Cohort("Soliu", 23, "Semo", 7890, new Date("march 10, 2002"));
 
+// Class keyword --> is a schema that can save many many many values   just as u can define function declaration & expression.
+// it can be defined as
+// 1. Class derclaration
+// 2. Class expression
+
+class Shape1 {
+    constructor (width, height) {
+        this.width = width;
+        this.height = height;
+
+        console.log(this)
+    }
+}
+
+const area = new Shape1(10, 2);
+
+// class expression
+// /i  --> anonymous
+
+const Shape = class {
+    constructor (width, height) {
+        this.width = width;
+        this.height = height;
+
+        console.log(this)
+    }
+}
+
+const area1 = new Shape(10, 2);
+
+// ii --> unanonymous
+const BigBoy = class Bigie {
+    constructor (name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // getter function
+    get him () {
+        return this.coder();
+    }
+
+    // method
+    coder() {
+        return {name : this.name, age: this.age}
+    }
+}
+
+const instance = new BigBoy("Paiko", 40);
+console.log(instance.him);
+
+const School = class Academy {
+    constructor(name, location) {
+        this.name =name;
+        this.location =location;
+    }
+
+    get principal() {
+        return this.seniorBoy()
+    }
+
+    seniorBoy() {
+        return {name: this.name, location: this.location}
+    }
+}
+
+const headMaster = new School ("Hayzed", "PEGAMUT");
+console.log(headMaster.principal);
+
+
+const RectShape = class RectShape {
+    constructor(lenght, breadth) {
+        this.lenght = lenght;
+        this.breadth = breadth;
+    }
+
+    get area () {
+        return this.calc()
+    }
+
+    calc() {
+      
+        return this.breadth * this.lenght
+    }
+
+}
+
+const square = new RectShape (10, 10)
+console.log(square.area)
