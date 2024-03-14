5a858cf640ab397711e1a8d0244f3aaec21db487
Dev-Narpheesah <adeolanafisat81@gmail.com>
2024-03-14 14:22:57 +0100

    update

---
+++ form-validation/app.js
@@ -0,0 +1,73 @@
+const form = document.getElementById("form");
+const password1EL = document.getElementById("password1");
+const password2EL = document.getElementById("password2");
+const messageContainer = document.querySelector(".message-container");
+const message = document.getElementById("message");
+
+let isValid = false;
+let passwordMatch = false;
+
+const validateForm = () => {
+  isValid = form.checkVisibility();
+  console.log(isValid);
+
+  // style message for an error
+  if (!isValid) {
+    message.textContent = "Please fill out the field";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
+    return;
+  }
+
+  // check to see if password match
+
+  if (password1EL.value === password2EL.value) {
+    passwordMatch = true;
+    password1EL.style.borderColor = "green";
+    password2EL.style.borderColor = "green";
+  } else {
+    passwordMatch = false;
+    message.textContent = "Make sure password match.";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
+    password1EL.style.borderColor = "red";
+    password2EL.style.borderColor = "red";
+    return
+  }
+
+
+//   if form is valid and d password match return success message
+
+  if(isValid && passwordMatch) {
+    message.textContent = "Successfully registered";
+    message.style.color = "green";
+    messageContainer.style.borderColor = "green";
+    return
+  }
+};
+
+const storeFormData = () => {
+    const user = {
+        name:form.name.value,
+        phone:form.phone.value,
+        email:form.email.value,
+        website:form.website.value,
+        password:form.password.value
+    };
+    console.log(user)
+}
+
+const processFormData = (e) => {
+    e.preventDefault();
+    console.log(e);
+    
+    validateForm();
+
+    // submit data if valid
+    if(isValid && passwordMatch) {
+        
+    }
+
+};
+
+form.addEventListener("submit", processFormData);
